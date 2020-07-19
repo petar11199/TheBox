98c8e1fc74b87988e2dae0eedc6a13cd98b91db7
Petar Marjanovic <petarmarjanovic12@yahoo.com>
2020-07-19 21:48:08 +0200

    Add files via upload

---
+++ js/ui.js
@@ -0,0 +1,29 @@
+const centerBoxEl = document.querySelector('.center-box')
+const uiWrapper = document.querySelector('.ui-wrapper')
+const closeInfoButton = document.querySelector('.close-info')
+
+setTimeout(() => {
+  uiWrapper.classList.remove('initializeApp')
+}, 1000);
+
+export default class UI {
+
+  constructor () {}
+
+  openInfo (closeInfoCallback) {
+    centerBoxEl.style.display = 'none'
+    closeInfoButton.style.display = 'block'
+    uiWrapper.classList.add('more-info-opened')
+
+    closeInfoButton.addEventListener('click', () => {
+      closeInfoCallback()
+      this.closeInfo()
+    }, false)
+  }
+
+  closeInfo () {
+    centerBoxEl.style.display = 'block'
+    closeInfoButton.style.display = 'none'
+    uiWrapper.classList.remove('more-info-opened')
+  }
+}
